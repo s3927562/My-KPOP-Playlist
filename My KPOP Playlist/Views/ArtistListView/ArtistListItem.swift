1212c670b3b03151d8638a4d96a66faaa5bb7cf8
s3927562 <s3927562@rmit.edu.vn>
2023-07-31 03:31:56 -0700

    July 31st, 2023

--- My KPOP Playlist/Views/ArtistListView/ArtistListItem.swift
+++ My KPOP Playlist/Views/ArtistListView/ArtistListItem.swift
@@ -1,9 +1,14 @@
-//
-//  ArtistNavigate.swift
-//  My KPOP Playlist
-//
-//  Created by Tung Tran Thanh on 25/07/2023.
-//
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2023B
+ Assessment: Assignment 1
+ Author: Tran Thanh Tung
+ ID: s3927562
+ Created date: 25/07/2023
+ Last modified: 31/07/2023
+ Acknowledgement: RMIT University, COSC2659 Course, Week 1 - 5 Lecture Slides & Videos
+ */
 
 import SwiftUI
 
