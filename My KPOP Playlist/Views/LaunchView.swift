f23d3d1b5869dd802c98e417f8fce81e65b5f427
s3927562 <s3927562@rmit.edu.vn>
2023-08-02 21:28:16 +0700

    August 2nd, 2023, After Hours

--- My KPOP Playlist/Views/LaunchView.swift
+++ My KPOP Playlist/Views/LaunchView.swift
@@ -6,12 +6,18 @@
  Author: Tran Thanh Tung
  ID: s3927562
  Created date: 01/08/2023
- Last modified: 01/08/2023
+ Last modified: 02/08/2023
  Acknowledgement: RMIT University, COSC2659 Course, Week 1 - 5 Lecture Slides & Videos
  ColorScheme | Apple Developer Documentation:
  https://developer.apple.com/documentation/swiftui/colorscheme
  preferredColorScheme(_:) | Apple Developer Documentation:
  https://developer.apple.com/documentation/swiftui/view/preferredcolorscheme(_:)
+ combine - SwiftUI: Random "Extra argument in call" error - Stack Overflow:
+ https://stackoverflow.com/questions/61178868/swiftui-random-extra-argument-in-call-error
+ alert(_:isPresented:presenting:actions:message:) | Apple Developer Documentation:
+ https://developer.apple.com/documentation/swiftui/view/alert(_:ispresented:presenting:actions:message:)-8584l
+ animation(_:value:) | Apple Developer Documentation:
+ https://developer.apple.com/documentation/swiftui/view/animation(_:value:)
  */
 import SwiftUI
 
@@ -20,36 +26,72 @@ struct LaunchView: View {
     @AppStorage("firstLaunch") private var firstLaunch = true // Set first launch status
     @AppStorage("darkMode") private var darkMode = false // Store dark mode settings
     @State private var launchView = true
+    @State private var infoAlert = false
     
     var body: some View {
         ZStack{
             if launchView {
                 VStack {
-                    Text("Insert RMIT Icon")
-                        .font(.largeTitle)
-                    Text("Insert App Icon")
-                        .font(.title)
-                    Text("App Name")
-                        .font(.title2)
-                    Text("Insert Slogan")
-                        .font(.title3)
-                    Text("time")
-                    Button {
-                        launchView = false
-                    } label: {
+                    // Images
+                    Group {
+                        Image(colorScheme == .light ? "rmit-logo-black" : "rmit-logo-white")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Spacer()
+                        
+                        Image("app-icon")
+                        Spacer()
+                    }
+                    
+                    // Texts
+                    Group {
+                        Text("My KPOP Playlist")
+                            .font(.largeTitle)
+                        Spacer()
+                        
+                        Text("I'm still thinking of a slogan")
+                        Spacer()
+                    }
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    
+                    // Buttons
+                    Group {
+                        Button {
+                            launchView = false
+                        } label: {
+                            HStack {
+                                Text("Go!")
+                                Image(systemName: "arrow.right")
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        Spacer()
+                        
                         HStack {
-                            Text("Go!")
-                            Image(systemName: "arrow.right")
+                            Spacer()
+                            Button {
+                                infoAlert = true
+                            } label: {
+                                Image(systemName: "info.circle.fill")
+                                    .symbolRenderingMode(.palette)
+                                    .foregroundStyle(Color.white, Color.accentColor)
+                            }
                         }
                     }
-                    .buttonStyle(.borderedProminent)
-                    .controlSize(.large)
                 }
-
+                .padding()
+                .alert("Information", isPresented: $infoAlert) {} message: {
+                    VStack {
+                        Text("Name: Tran Thanh Tung\nStudent ID: s3927562\nApplication Name: My KPOP Playlist")
+                    }
+                }
             } else {
                 ArtistListView()
             }
         }
+        .animation(.default, value: launchView)
         .onAppear() {
             // Set color scheme state variable on first launch based on system settings
             if firstLaunch {
